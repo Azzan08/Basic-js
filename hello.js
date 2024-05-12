0f46dea015ecd7c0f45a77c3c6a043309204174b
Azzan08 <azzanmuhtasim@gmail.com>
2024-05-12 20:05:40 +0600

    java1

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
 console.log(35);
+console.log('sectence');
+var desktop =true;
+console.log(desktop);
+
+var processor =18000;
+var display =12000;
+console.log(processor + display);
+
+var num1 =25;
+var num2 =37;
+var total =num1 + num2;
+console.log(total);
+
+var num5 =23.5;
+console.log(Math.round(num5));
+var num6 ='My name is khan.I grew up in a small village';
+console.log(num6.indexOf("in"));
